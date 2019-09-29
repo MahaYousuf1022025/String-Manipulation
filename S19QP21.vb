aea6ec6083fbe2ed13064c647624c1c097fe0fb4
MahaYousuf1022025 <55849128+MahaYousuf1022025@users.noreply.github.com>
2019-09-29 21:47:27 +0500

    Add files via upload

---
+++ S19QP21.vb
@@ -0,0 +1,44 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim InString, NewString As String
+        Dim Index As Integer
+        Dim AfterSpace As Boolean
+        Dim NextChar As Char
+        Const Space = " "
+
+        AfterSpace = False
+        NewString = " "
+
+        Console.WriteLine(" Enter new string: ")
+        InString = Console.ReadLine
+
+        For Index = 1 To Len(InString)
+            NextChar = Mid(InString, Index, 1)
+
+            If AfterSpace = True Then
+                If NextChar <> Space Then
+                    NewString = NewString & NextChar
+                    AfterSpace = False
+
+
+                End If
+            Else
+                NewString = NewString & NextChar
+                If NextChar = Space Then
+                    AfterSpace = True
+                End If
+
+            End If
+
+        Next
+        Console.ReadKey()
+
+
+
+
+
+    End Sub
+
+
+End Module
