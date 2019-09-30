15bd81f53f0871b138818625f23f33b31f659173
MahaYousuf1022025 <55849128+MahaYousuf1022025@users.noreply.github.com>
2019-09-30 23:26:46 +0500

    Add files via upload
    string manipulation of merging strings

---
+++ merging strings.vb
@@ -0,0 +1,40 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim S1, S2, FirstWord, SecWord As String
+        Dim SP As String
+
+        S1 = " "
+        S2 = " "
+        FirstWord = " "
+        SecWord = " "
+        SP = 0
+
+        Console.WriteLine(" Enter first string: ")
+        S1 = Console.ReadLine
+        Console.WriteLine(" Enter Second string: ")
+        S2 = Console.ReadLine
+
+        SP = InStr(S1, " ")
+        FirstWord = Left(S1, SP - 1)
+
+        SP = InStr(S2, " ")
+        SecWord = Right(S2, Len(S2) - SP)
+
+        Console.WriteLine("Final String= " & FirstWord & " " & SecWord)
+        Console.ReadKey()
+
+
+
+
+
+
+
+
+
+
+
+
+    End Sub
+
+End Module
