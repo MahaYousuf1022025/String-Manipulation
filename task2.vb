2ff155af81369ac4915d7acc2d8f63c2691e1500
MahaYousuf1022025 <55849128+MahaYousuf1022025@users.noreply.github.com>
2019-10-01 23:35:51 +0500

    Add files via upload

---
+++ task2.vb
@@ -0,0 +1,41 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Str1, NextChar As String
+        Dim counter, Alphabets, NonAlphabets As Integer
+
+        Str1 = ""
+        Alphabets = 0
+        NonAlphabets = 0
+        NextChar = ""
+
+        Console.Write("Enter string: ")
+        Str1 = Console.ReadLine
+
+
+        For counter = 1 To Len(Str1)
+            NextChar = Mid(Str1, counter, 1)
+            If LCase(NextChar) >= "a" And LCase(NextChar) <= "z" Then
+                Alphabets = Alphabets + 1
+            Else
+                NonAlphabets = NonAlphabets + 1
+
+            End If
+
+        Next
+
+        Console.WriteLine("Alphabets= " & Alphabets)
+        Console.WriteLine("NonAlphabets= " & NonAlphabets)
+        Console.ReadKey()
+
+
+
+
+
+
+
+
+
+    End Sub
+
+End Module
