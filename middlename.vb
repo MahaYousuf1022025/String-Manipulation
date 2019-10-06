41214f296360fbd53b4a9c709d1e7ba86a983195
MahaYousuf1022025 <55849128+MahaYousuf1022025@users.noreply.github.com>
2019-10-06 19:03:35 +0500

    Add files via upload

---
+++ middlename.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim s1 As String
+        Dim firstword As String
+        Dim SP As Integer
+        Dim Middleword As String
+        Dim SP2 As Integer
+
+
+
+        SP = 0
+        s1 = Console.ReadLine
+
+        SP = InStr(s1, " ")
+        firstword = Right(s1, Len(s1) - SP)
+
+        SP2 = InStr(firstword, " ")
+        Middleword = Left(firstword, SP2 - 1)
+
+
+        
+
+        Console.WriteLine(Middleword)
+        Console.ReadKey()
+    End Sub
+
+End Module
