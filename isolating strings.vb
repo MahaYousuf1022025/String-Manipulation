8a7a9e83e25082c0a9fcd6658b5e531ed4a860d8
MahaYousuf1022025 <55849128+MahaYousuf1022025@users.noreply.github.com>
2019-10-09 00:15:21 +0500

    Add files via upload

---
+++ isolating strings.vb
@@ -0,0 +1,45 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim s1 As String
+        Dim last As String
+        Dim SP As Integer
+        Dim Middleword As String
+        Dim SP2 As Integer
+        Dim firstwords As String
+        Dim lastwords As String
+
+
+
+        SP = 0
+        s1 = Console.ReadLine
+
+        SP = InStr(s1, " ")
+        Firstwords = Left(s1, SP - 1)
+
+        SP = InStr(s1, " ")
+        last = Right(s1, Len(s1) - SP)
+
+        SP2 = InStr(last, " ")
+        Middleword = Left(last, SP2 - 1)
+
+        SP2 = InStr(last, " ")
+        lastwords = Right(last, Len(last) - SP2)
+
+
+
+        
+
+        Console.WriteLine(firstwords)
+        Console.WriteLine(Middleword)
+        Console.WriteLine(lastwords)
+
+
+
+
+
+
+        Console.ReadKey()
+    End Sub
+
+End Module
