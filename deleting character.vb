7c2e2d391ae98df304edeed161b07fb09e3da2a5
MahaYousuf1022025 <55849128+MahaYousuf1022025@users.noreply.github.com>
2019-10-13 15:10:57 +0500

    Add files via upload

---
+++ deleting character.vb
@@ -0,0 +1,35 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str1, deletechar, nextchar, newstring As String
+        Dim counter As Integer
+
+        str1 = ""
+        deletechar = ""
+        counter = 0
+        newstring = ""
+
+        Console.WriteLine("please enter your string: ")
+        str1 = Console.ReadLine
+        Console.WriteLine("Enter the character you want to remove")
+        deletechar = Console.ReadLine
+        For counter = 1 To Len(str1)
+            nextchar = Mid(str1, counter, 1)
+            If nextchar <> deletechar Then
+                newstring = newstring & nextchar
+
+
+
+            End If
+
+        Next
+        Console.WriteLine(newstring)
+        Console.ReadKey()
+
+
+
+
+
+    End Sub
+
+End Module
